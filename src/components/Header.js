b1a7fdba78a3d802d355d7edf89fc4efbab8b46a
«Aleksei <Aleksmjr97@gmail.com>
2025-06-09 14:21:19 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
-// components 	Переиспользуемые штуки — например, заголовок, кнопка и т.п.
+// // components 	Переиспользуемые штуки — например, заголовок, кнопка и т.п.
 
-// Компонент шапки с навигацией
-export function Header() {
-  const nav = document.createElement('nav');
+// // Компонент шапки с навигацией
+// export function Header() {
+//   const nav = document.createElement('nav');
 
-  // Важный момент: onclick вызывает navigateTo, чтобы не было перезагрузки
-  nav.innerHTML = `
-    <a class="header-nav" href="/" onclick="event.preventDefault(); navigateTo('/')">Главная</a> 
-    <a class="header-nav" href="/about" onclick="event.preventDefault(); navigateTo('/about')">О нас</a>
-    <a class="header-nav" href="/contacts" onclick="event.preventDefault(); navigateTo('/contacts')">Контакты</a>
-    <a class="header-nav" href="/contacts" onclick="event.preventDefault(); navigateTo('/posts')">Посты</a>
-  `;
+//   // Важный момент: onclick вызывает navigateTo, чтобы не было перезагрузки
+//   nav.innerHTML = `
+//     <a class="header-nav" href="/" onclick="event.preventDefault(); navigateTo('/')">Главная</a>
+//     <a class="header-nav" href="/about" onclick="event.preventDefault(); navigateTo('/about')">О нас</a>
+//     <a class="header-nav" href="/contacts" onclick="event.preventDefault(); navigateTo('/contacts')">Контакты</a>
+//     <a class="header-nav" href="/contacts" onclick="event.preventDefault(); navigateTo('/posts')">Посты</a>
+//   `;
 
-  return nav;
-}
+//   return nav;
+// }
+
+let str = '1';
+str = 2;
+console.log(str.toLowerCase());
